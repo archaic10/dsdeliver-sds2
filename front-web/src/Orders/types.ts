63ddb85e576702151816181fbe00142d88ff1b7e
Vitor Nunes <vitor.franca.9400@gmail.com>
2021-01-09 22:20:15 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -11,3 +11,12 @@ export type OrderLocationData = {
     latitude: number;
     longitude: number;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export  type OrderPayLoad = {
+    products: ProductId[];
+} & OrderLocationData;
+
